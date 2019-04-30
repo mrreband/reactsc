57548af169471fcaf0b1ba594c0b0b5b070ca2cb
mrreband <mr@reband.org>
2019-04-30 07:02:49 -0500

    adding a songs array of objects, formatting using prettier extension

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import { PlayButton, Timer } from 'react-soundplayer/components';
-
-// it's just an alias for `withSoundCloudAudio` but makes code clearer
 import { withCustomAudio } from 'react-soundplayer/addons';
 
-// audio source
-const streamUrl = 'http://feeds.soundcloud.com/stream/610950675-michael-reband-waves.mp3';
+var songs = [
+	{
+		streamUrl: 'http://feeds.soundcloud.com/stream/610950675-michael-reband-waves.mp3',
+		trackTitle: 'Waves'
+  },
+	{
+		streamUrl: 'http://feeds.soundcloud.com/stream/423945819-michael-reband-up-1.mp3',
+		trackTitle: 'Up'
+	}
+];
 
-// some track meta information
-const trackTitle = 'Great song by random artist';
+const AWSSoundPlayer = withCustomAudio((props) => {
+	const { trackTitle } = props;
 
-const AWSSoundPlayer = withCustomAudio(props => {
-  const { trackTitle } = props;
-
-  return (
-    <div>
-      <PlayButton {...props} />
-      <h2 style={{color:'blue'}}>{trackTitle}</h2>
-      <Timer {...props} />
-    </div>
-  );
+	return (
+		<div>
+			<PlayButton {...props} />
+			<h2 style={{ color: 'blue' }}>{trackTitle}</h2>
+			<Timer {...props} />
+		</div>
+	);
 });
 
 class App extends React.Component {
-  render() {
-    return (
-      <AWSSoundPlayer
-        streamUrl={streamUrl}
-        trackTitle={trackTitle}
-        preloadType="auto" />
-    );
-  }
+	render() {
+		return <AWSSoundPlayer streamUrl={songs[1].streamUrl} trackTitle={songs[1].trackTitle} preloadType="auto" />;
+	}
 }
 
 export default App;
