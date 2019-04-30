627f4d03241d7d71678d8318d17efa2e16674c00
Carlos Orue <carlos.orue@gmail.com>
2019-04-30 13:49:45 -0500

    update package.jason and .gitignore

--- src/rssFeed.js
+++ src/rssFeed.js
@@ -4,14 +4,15 @@ const Feed = require("rss-to-json");
 Feed.load(
   "http://feeds.soundcloud.com/users/soundcloud:users:31432799/sounds.rss",
   function GetData(err, rss) {
-    // console.log(rss);
-    let fileContent = rss;
-    fs.writeFile("./sample.json", JSON.stringify(fileContent), err => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log("File has been created");
-    });
+    // let fileContent = rss;
+    // fs.writeFile("./dataFeed.json", JSON.stringify(fileContent), err => {
+    //   if (err) {
+    //     console.error(err);
+    //     return;
+    //   }
+      // console.log("File has been created");
+      console.log(rss);
+
+    // });
   }
 );
