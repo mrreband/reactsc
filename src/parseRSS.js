1bd2a1efe97168bbdd421a09bbba33261fb862ec
mrreband <mr@reband.org>
2019-04-29 20:45:08 -0500

    adding a blocking call to parseRSS

--- src/parseRSS.js
+++ src/parseRSS.js
@@ -1,19 +1,13 @@
 let Parser = require('rss-parser');
 let parser = new Parser();
 
-(async () => {
-	var songs = [];
+async function getSongs () {
 	let feed = await parser.parseURL('http://feeds.soundcloud.com/users/soundcloud:users:31432799/sounds.rss');
-	console.log(feed.title);
+	return feed.items;
+};
 
-	feed.items.forEach((item) => {
-		console.log(item);
-		songs =
-			songs +
-			{
-				title: item.title,
-				url: item.link
-			};
+getSongs().then(function(value) {
+    value.forEach((item) => {
+        console.log(item);
     });
-	return songs;
-})();
+});
