8194b1d21c69715f5e8c79459703258c3c26335d
Carlos Orue <carlos.orue@icloud.com>
2019-05-02 08:27:23 -0500

    add header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer mt-auto bg-dark py-3">
+        <div className="container">
+          © 2019 Copyright:
+          <a href="/"> MReband.com</a>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+class Header extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            Michael Reband
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
