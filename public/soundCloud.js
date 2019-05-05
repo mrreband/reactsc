206ab23b4be1a19507053e638ad415e4d2bfcf50
mrreband <mr@reband.org>
2019-05-05 14:26:49 -0500

    making getSongs into a separate function

--- public/soundCloud.js
+++ public/soundCloud.js
@@ -3,25 +3,32 @@ function buildSongUrl(id, userName, songName) {
   return `${basePath}${id}-${userName}-${songName}.mp3`
 }
 
-$(document).ready(function(){  
-    var player = SC.Widget($('iframe.sc-widget')[0]);    
-    player.bind(SC.Widget.Events.READY, function() {
-      player.getSounds(function(songs) {
-        songs.forEach(function(item){
-          var id = item["id"];
-          var songName = item["permalink"];
-          var userName = item["user"]["permalink"];
-          var songUrl = buildSongUrl(id, userName, songName);
+function getSongs() {
+  var songs = [];
+  var player = SC.Widget($('iframe.sc-widget')[0]);    
+  player.bind(SC.Widget.Events.READY, function() {
+    player.getSounds(function(songs) {
+      songs.forEach(function(item){
+        var id = item["id"];
+        var songName = item["permalink"];
+        var userName = item["user"]["permalink"];
+        var songUrl = buildSongUrl(id, userName, songName);
 
-          var song = {
-            id: id, 
-            title: item["title"],
-            song_url: songUrl, 
-            artwork_url: item["artwork_url"],
-            waveform_url: item["waveform_url"]
-          };
-          console.log(song);
-        });
+        var song = {
+          id: id, 
+          title: item["title"],
+          song_url: songUrl, 
+          artwork_url: item["artwork_url"],
+          waveform_url: item["waveform_url"]
+        };
+        songs.push(song);
+        console.log(song);
       });
-    })
+    });
+  })
+  return songs;
+}
+
+$(document).ready(function(){  
+  var songs = getSongs();
 });
