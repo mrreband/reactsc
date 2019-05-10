5dc0e5defe672c79822e3bdc1d0d20e24f2b36f1
mrreband <mr@reband.org>
2019-05-10 14:49:24 -0500

    using `this.props.playing` to drive class changes for "play" and "active"

--- src/components/PlayButton.js
+++ src/components/PlayButton.js
@@ -2,14 +2,8 @@ import React, { Component } from 'react'
 
 
 class PlayButton extends Component {
-  constructor(props){
-    super(props);
-    this.state = {addClass: false}
-  }
-
   toggle = (e) => {
     e.preventDefault();
-    this.setState({ addClass: !this.state.addClass })
     if (this.props.playing === true)
     {
       this.props.pauseSound(this.props.id)
@@ -18,20 +12,15 @@ class PlayButton extends Component {
     }
   }
 
-          // onClick={this.onClick}
-        // key={`${this.props.id}`}
-        // id={`PlayButton_${this.props.id}`}
-        // type="button">{this.state.addClass}
-
   render() {
     let playClass =["play"];
-    if(this.state.addClass){
+    if(this.props.playing === true){
       playClass.push("active")
     }
     return (
       <div className={playClass.join(' ')}
         onClick={this.toggle.bind(this)}
-      >{this.state.addClass}
+      >
       </div>
     )
   }
