27be80c6cb6c2feebedbc926a57acf7e66d2941a
mrreband <mr@reband.org>
2019-05-13 21:13:36 -0500

    should be soundcloud.svg not soundcloud3.svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import spotify from "../img/spotify.svg";
-import soundcloud3 from "../img/soundcloud3.svg";
+import soundcloud from "../img/soundcloud.svg";
 import rss from "../img/rss.svg";
 // import itunes from "../img/itunes.svg";
 
@@ -9,25 +9,37 @@ class Header extends React.Component {
     return (
       <header>
         <div className="container">
-        <div className="logo">
+          <div className="logo">
             <h1>mr.Reband</h1>
-        </div>
-        <div className="bottom">
-          <ul className="feeds">
-            <li>
-                <a target="_blank" rel="noopener noreferrer" href="https://open.spotify.com/artist/2qFU2QneC3YZ32Qd6vGFAj?si=ZP_c9L3LQWCEC_lG_vQk8A">
-                  <img src={spotify} alt=""/>
+          </div>
+          <div className="bottom">
+            <ul className="feeds">
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://open.spotify.com/artist/2qFU2QneC3YZ32Qd6vGFAj?si=ZP_c9L3LQWCEC_lG_vQk8A"
+                >
+                  <img src={spotify} alt="" />
                 </a>
-                <a target="_blank" rel="noopener noreferrer" href="https://soundcloud.com/michael-reband">
-                  <img src={soundcloud3} alt=""/>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://soundcloud.com/michael-reband"
+                >
+                  <img src={soundcloud} alt="" />
                 </a>
-                <a target="_blank" rel="noopener noreferrer" href="http://feeds.soundcloud.com/users/soundcloud:users:31432799/sounds.rss" >
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="http://feeds.soundcloud.com/users/soundcloud:users:31432799/sounds.rss"
+                >
                   <img src={rss} alt="" />
                 </a>
                 {/* <img src={itunes} alt="" /> */}
-            </li>
-          </ul>
-        </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </header>
     );
