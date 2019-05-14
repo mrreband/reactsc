771465505f95cd06a150e057a8e5859a939cf453
mrreband <mr@reband.org>
2019-05-13 20:41:00 -0500

    more svgs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import spotify from "../img/spotify.svg";
-
+import soundcloud3 from "../img/soundcloud3.svg";
+import rss from "../img/rss.svg";
+// import itunes from "../img/itunes.svg";
 
 class Header extends React.Component {
   render() {
@@ -13,9 +15,16 @@ class Header extends React.Component {
         <div className="bottom">
           <ul className="feeds">
             <li>
-                <a href="https://open.spotify.com/artist/2qFU2QneC3YZ32Qd6vGFAj?si=ZP_c9L3LQWCEC_lG_vQk8A">
+                <a target="_blank" rel="noopener noreferrer" href="https://open.spotify.com/artist/2qFU2QneC3YZ32Qd6vGFAj?si=ZP_c9L3LQWCEC_lG_vQk8A">
                   <img src={spotify} alt=""/>
                 </a>
+                <a target="_blank" rel="noopener noreferrer" href="https://soundcloud.com/michael-reband">
+                  <img src={soundcloud3} alt=""/>
+                </a>
+                <a target="_blank" rel="noopener noreferrer" href="http://feeds.soundcloud.com/users/soundcloud:users:31432799/sounds.rss" >
+                  <img src={rss} alt="" />
+                </a>
+                {/* <img src={itunes} alt="" /> */}
             </li>
           </ul>
         </div>
