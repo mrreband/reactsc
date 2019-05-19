94a538d61e87c62d731e95b8111744b10dc18aed
mrreband <mr@reband.org>
2019-05-19 08:58:49 -0500

    moving most of the midi logic up to Sound.js

--- src/components/Sound.js
+++ src/components/Sound.js
@@ -4,29 +4,68 @@ import SoundTitle from "./SoundTitle.js";
 import SoundTimer from "./SoundTimer.js";
 import ProgressBar from "./ProgressBar.js";
 import MidiCanvas from "./MidiCanvas.js";
+import MidiSequence from "../midi/Waves.json";
 
 export default class Sound extends Component {
+  initMidiNotes() {
+    var midiNotes = {};
+    for (var i = 0; i < 128; i++) {
+      midiNotes[i] = false;
+    }
+    return midiNotes;
+  }
+
   constructor(props) {
     super(props);
+
+    var midiNotes = this.initMidiNotes();
     this.htmlPlayer = React.createRef();
     this.state = {
       duration: this.props.duration,
-      currentTime: 0
-    }
+      currentTime: 0,
+      currentTicks: 0,
+      previousTicks: 0,
+      midiNotes: midiNotes,
+      MidiSequence
+    };
   }
 
   playSound = () => {
     this.htmlPlayer.current.play();
     this.props.updateCurrentPlayer(this.props.id);
-  }
-  
+  };
+
   pauseSound = () => {
     this.htmlPlayer.current.pause();
     this.props.updateCurrentPlayer("");
-  }
+  };
 
   updateCurrentTime() {
-    this.setState({currentTime: this.htmlPlayer.current.currentTime});
+    this.setState({ previousTicks: this.state.currentTicks });
+    console.log(this.state.currentTicks);
+
+    this.setState({
+      currentTime: this.htmlPlayer.current.currentTime,
+      currentTicks: this.htmlPlayer.current.currentTime * 1920
+    });
+
+    var currentNotes = {};
+    this.state.MidiSequence.filter(
+      midiNote =>
+        this.state.previousTicks <= midiNote.tick &&
+        midiNote.tick <= this.state.currentTicks
+    ).map(function(note) {
+      currentNotes[note.noteNumber] = note.noteOn;
+    });
+
+    var newMidiNotes = {};
+    Object.keys(this.state.midiNotes).map(
+      (key, value) =>
+        (newMidiNotes[key] =
+          key in currentNotes ? currentNotes[key] : this.state.midiNotes[key])
+    );
+
+    this.setState({ midiNotes: newMidiNotes });
   }
 
   render() {
@@ -38,11 +77,12 @@ export default class Sound extends Component {
             playing={this.props.active}
             playSound={this.playSound}
             pauseSound={this.pauseSound}
-            />
+          />
           <SoundTitle title={this.props.title} />
           <SoundTimer
             duration={this.state.duration}
-            currentTime={this.state.currentTime} />
+            currentTime={this.state.currentTime}
+          />
           <audio
             ref={this.htmlPlayer}
             preload="none"
@@ -54,15 +94,16 @@ export default class Sound extends Component {
           >
             <source src={this.props.url} type="audio/mpeg" />
           </audio>
-
         </div>
-        <ProgressBar 
-            duration={this.state.duration}
-            currentTime={this.state.currentTime} />
-        <MidiCanvas 
+        <ProgressBar
+          duration={this.state.duration}
+          currentTime={this.state.currentTime}
+        />
+        <MidiCanvas
           key={this.props.id}
           id={`MidiCanvas_${this.props.id}`}
-          currentTime={this.state.currentTime} />
+          midiNotes={this.state.midiNotes}
+        />
       </div>
     );
   }
