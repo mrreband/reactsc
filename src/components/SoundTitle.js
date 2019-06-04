abec25ef85f2f93e765af4e81fd7291f200c166f
mrreband <mr@reband.org>
2019-06-03 20:02:37 -0500

    more formatting

--- src/components/SoundTitle.js
+++ src/components/SoundTitle.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class SoundTitle extends Component {
   render() {
-    return <div className="SoundTitle SoundElement">{this.props.title}</div>;
+    return (
+      <div className="SoundTitle SoundElement">
+        <h5>{this.props.title}</h5>
+      </div>
+    );
   }
 }
