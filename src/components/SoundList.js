24e237955cf81bc1b3e3c1abd25d3bd1095db2fa
mrreband <mr@reband.org>
2019-08-04 19:04:26 -0500

    moving sound array up into SoundList.state

--- src/components/SoundList.js
+++ src/components/SoundList.js
@@ -5,19 +5,29 @@ import Sound from "./Sound";
 class SoundList extends React.Component {
   constructor() {
     super();
+
+    var sounds = SoundData.map(sound => ({
+      key: sound.id,
+      id: sound.id,
+      title: sound.title,
+      url: sound.url,
+      duration: sound.duration
+    }));
+
     this.state = {
       SoundData,
+      sounds: sounds,
       currentPlayerId: ""
     };
   }
 
-  updateCurrentPlayer = (id) => {
-    this.setState({currentPlayerId: id.toString()});
-  }
+  updateCurrentPlayer = id => {
+    this.setState({ currentPlayerId: id.toString() });
+  };
 
   pauseAllOtherTracks = e => {
     for (var i = 1; i < this.state.SoundData.length; i++) {
-      var targetPlayerId = 'SoundData_' + i.toString();
+      var targetPlayerId = "SoundData_" + i.toString();
       if (targetPlayerId !== e.target.id) {
         var player = document.getElementById(targetPlayerId);
         player.pause();
@@ -31,7 +41,7 @@ class SoundList extends React.Component {
     currentPlayer.currentTime = 0;
 
     var currentPlayerId = Number(e.target.id.match(/\d+/)[0]);
-    this.setState({currentPlayerId: (currentPlayerId + 1).toString()});
+    this.setState({ currentPlayerId: (currentPlayerId + 1).toString() });
 
     var nextPlayerId = "SoundData_" + (currentPlayerId + 1).toString();
     var nextPlayer = document.getElementById(nextPlayerId);
@@ -39,22 +49,24 @@ class SoundList extends React.Component {
   };
 
   render() {
+    var sounds = this.state.sounds.map(sound => (
+      <Sound
+        active={sound.id.toString() === this.state.currentPlayerId}
+        key={sound.id}
+        id={sound.id}
+        title={sound.title}
+        url={sound.url}
+        duration={sound.duration}
+        pauseAllOtherTracks={this.pauseAllOtherTracks}
+        startNextTrack={this.startNextTrack}
+        updateCurrentPlayer={this.updateCurrentPlayer}
+      />
+    ));
+
     return (
       <div className="musics">
         <h2 className="container">Piano Podcast</h2>
-        {this.state.SoundData.map(sound => (
-          <Sound
-          active={sound.id.toString() === this.state.currentPlayerId}
-          key={sound.id}
-          id={sound.id}
-          title={sound.title}
-          url={sound.url}
-          duration={sound.duration}
-          pauseAllOtherTracks={this.pauseAllOtherTracks}
-          startNextTrack={this.startNextTrack}
-          updateCurrentPlayer={this.updateCurrentPlayer}
-          />
-        ))}
+        {sounds}
       </div>
     );
   }
