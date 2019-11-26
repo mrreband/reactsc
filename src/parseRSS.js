388edb7242a3e4584afce872020e08242a5f47fc
mrreband <mr@reband.org>
2019-11-26 09:27:15 -0600

    enclosure duration was 0 from one track in the rss feed, parse the itunes duration instead

--- src/parseRSS.js
+++ src/parseRSS.js
@@ -5,19 +5,22 @@ async function parseRss() {
   const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
   const RSS_URL =
     "https://feeds.soundcloud.com/users/soundcloud:users:31432799/sounds.rss";
-  var songs = [];
-  var i = 1;
+  let songs = [];
+  let i = 1;
 
   let feed = await parser.parseURL(CORS_PROXY + RSS_URL);
   console.log(feed.title);
 
   feed.items.forEach(item => {
-    var song = {
+    let duration =
+      parseInt(item["itunes"]["duration"].split(":")[1]) * 60 +
+      parseInt(item["itunes"]["duration"].split(":")[2]);
+    let song = {
       id: i,
       title: item["title"],
       description: item["itunes"]["subtitle"],
       url: item["enclosure"]["url"],
-      duration: Math.floor(item["enclosure"]["length"] / 40000),
+      duration: duration,
       publish_date: item["publish_date"]
     };
     songs.push(song);
