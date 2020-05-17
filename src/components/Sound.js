e3dab79421f3c7dbc5a6d8dac0f3f4ad85739ab2
mrreband <mr@reband.org>
2020-05-17 10:16:24 -0500

    adding active prop back to Sound (will remove the active state)

--- src/components/Sound.js
+++ src/components/Sound.js
@@ -5,51 +5,53 @@ import SoundTimer from "./SoundTimer.js";
 import ProgressBar from "./ProgressBar.js";
 
 export default class Sound extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      duration: this.props.duration,
-      active: this.props.active,
-      currentTime: 0,
+    constructor(props) {
+        super(props);
+        this.state = {
+            duration: this.props.duration,
+            active: this.props.active,
+            currentTime: 0,
+        };
+    }
+
+    updateCurrentTime() {
+        this.setState({ currentTime: this.htmlPlayer.current.currentTime });
+    }
+
+    updateCurrentProgress(pct) {
+        var newTime = Math.floor(pct * this.state.duration);
+        this.htmlPlayer.current.currentTime = newTime;
+    }
+
+    playPause = () => {
+        console.log(this.props.id);
+        this.setState({ active: !this.state.active });
+        this.props.playPause(this.props.id);
     };
-  }
 
-  updateCurrentTime() {
-    this.setState({ currentTime: this.htmlPlayer.current.currentTime });
-  }
-
-  updateCurrentProgress(pct) {
-    var newTime = Math.floor(pct * this.state.duration);
-    this.htmlPlayer.current.currentTime = newTime;
-  }
-
-  playPause = () => {
-    console.log(this.props.id);
-    this.setState({ active: !this.state.active });
-    this.props.playPause(this.props.id);
-  };
-
-  render() {
-    return (
-      <div>
-        <div className="Sound">
-          <PlayButton
-            id={`${this.props.id}`}
-            playing={this.state.active}
-            playPause={this.playPause}
-          />
-          <SoundTitle title={this.props.title} />
-          <SoundTimer
-            duration={this.state.duration}
-            currentTime={this.state.currentTime}
-          />
-        </div>
-        <ProgressBar
-          duration={this.state.duration}
-          currentTime={this.state.currentTime}
-          updateCurrentProgress={this.updateCurrentProgress.bind(this)}
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div className="Sound">
+                    <PlayButton
+                        id={`${this.props.id}`}
+                        playing={this.state.active && this.props.active}
+                        playPause={this.playPause}
+                    />
+                    <SoundTitle title={this.props.title} />
+                    <SoundTimer
+                        duration={this.state.duration}
+                        currentTime={this.state.currentTime}
+                    />
+                </div>
+                <ProgressBar
+                    duration={this.state.duration}
+                    currentTime={this.state.currentTime}
+                    updateCurrentProgress={this.updateCurrentProgress.bind(
+                        this
+                    )}
+                />
+            </div>
+        );
+    }
 }
--- src/components/SoundList.js
+++ src/components/SoundList.js
@@ -7,84 +7,93 @@ import { trackPromise } from "react-promise-tracker";
 import LoadingIndicator from "./LoadingIndicator";
 
 function getRssData() {
-  var songs = parseRss();
-  if (songs) {
-    return songs;
-  } else {
-    return SoundData;
-  }
+    var songs = parseRss();
+    if (songs) {
+        return songs;
+    } else {
+        return SoundData;
+    }
 }
 
 class SoundList extends React.Component {
-  constructor() {
-    super();
-    this.audioPlayer = React.createRef();
+    constructor() {
+        super();
+        this.audioPlayer = React.createRef();
 
-    this.state = {
-      SoundData: [],
-      Playlist: [],
-      currentPlayerId: "",
-    };
-  }
-
-  async componentWillMount() {
-    trackPromise(
-      getRssData().then((songs) => {
-        let playlist = getPlaylist(songs);
-        console.log(playlist);
-        this.setState({
-          SoundData: songs,
-          Playlist: playlist,
-        });
-      })
-    );
-    this.render();
-  }
-
-  updateCurrentPlayer = (id) => {
-    this.setState({ currentPlayerId: id.toString() });
-  };
-
-  setNextTrack = (id) => {
-    var nextId = (id + 1).toString();
-    this.updateCurrentPlayer(nextId);
-  };
-
-  playPause = (id) => {
-    console.log(id);
-    if (id === this.state.currentPlayerId) {
-      this.setState({ currentPlayerId: "" });
-    } else {
-      this.setState({ currentPlayerId: id.toString() });
-      this.audioPlayer.current.play();
+        this.state = {
+            SoundData: [],
+            Playlist: [],
+            currentPlayerId: "",
+        };
     }
-  };
 
-  render() {
-    return (
-      <div className="musics">
-        <h2>Piano Podcast</h2>
-        <LoadingIndicator />
-        <audio ref={this.audioPlayer}>
-          {this.state.SoundData.map((sound) => (
-            <source src={sound.url} type="audio/mpeg" />
-          ))}
-        </audio>
-        {this.state.SoundData.map((sound) => (
-          <Sound
-            active={sound.id.toString() === this.state.currentPlayerId}
-            currentPlayerId={this.state.currentPlayerId}
-            key={sound.id}
-            id={sound.id}
-            title={sound.title}
-            url={sound.url}
-            duration={sound.duration}
-            playPause={this.playPause}
-          />
-        ))}
-      </div>
-    );
-  }
+    async componentWillMount() {
+        trackPromise(
+            getRssData().then((songs) => {
+                let playlist = getPlaylist(songs);
+                console.log(playlist);
+                this.setState({
+                    SoundData: songs,
+                    Playlist: playlist,
+                });
+            })
+        );
+        this.render();
+    }
+
+    updateCurrentPlayer = (id) => {
+        if (this.state.currentPlayerId) {
+            const currentSongId = this.state.currentPlayerId;
+            const currentSong = this.state.SoundData.find(
+                (s) => s.id.toString() === currentSongId
+            );
+            currentSong.active = false;
+        }
+        this.setState({ currentPlayerId: id.toString() });
+    };
+
+    setNextTrack = (id) => {
+        var nextId = (id + 1).toString();
+        this.updateCurrentPlayer(nextId);
+    };
+
+    playPause = (id) => {
+        if (id.toString() === this.state.currentPlayerId) {
+            this.updateCurrentPlayer("");
+            this.audioPlayer.current.pause();
+        } else {
+            this.updateCurrentPlayer(id);
+            this.audioPlayer.current.play();
+        }
+    };
+
+    render() {
+        return (
+            <div className="musics">
+                <h2>Piano Podcast</h2>
+                <LoadingIndicator />
+                <audio ref={this.audioPlayer}>
+                    {this.state.SoundData.map((sound) => (
+                        <source src={sound.url} type="audio/mpeg" />
+                    ))}
+                </audio>
+                {this.state.SoundData.map((sound) => (
+                    <Sound
+                        active={
+                            sound.id.toString() === this.state.currentPlayerId
+                        }
+                        currentPlayerId={this.state.currentPlayerId}
+                        key={sound.id}
+                        id={sound.id}
+                        title={sound.title}
+                        url={sound.url}
+                        duration={sound.duration}
+                        playPause={this.playPause}
+                    />
+                ))}
+            </div>
+        );
+    }
 }
 
 export default SoundList;
