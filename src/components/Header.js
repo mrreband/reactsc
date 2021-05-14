dd14a7064096df40c2d44c412b26938ec707cb70
mrreband <mr@reband.org>
2021-05-13 21:26:01 -0500

    removing google play from the links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import googlepodcasts from "../img/googlepodcasts.svg";
 import itunes from "../img/itunes.svg";
 import logo from "../img/mr.svg";
 import rss from "../img/rss.svg";
@@ -37,13 +36,6 @@ class Header extends React.Component {
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
-                  href="https://play.google.com/music/m/I43twkhz5ocrkrcr4rky3j3i2ly?t=mrreband_piano_podcast"
-                >
-                  <img src={googlepodcasts} alt="" />
-                </a>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
                   href="https://soundcloud.com/michael-reband"
                 >
                   <img src={soundcloud} alt="" />
@@ -55,7 +47,6 @@ class Header extends React.Component {
                 >
                   <img src={rss} alt="" />
                 </a>
-                {/* <img src={itunes} alt="" /> */}
               </li>
             </ul>
           </div>
