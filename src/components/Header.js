9b0cc58c1efc5baa7cecdd213ae1420b1b943fed
mrreband <mr@reband.org>
2022-11-04 09:28:48 -0500

    refactor, cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,15 @@
 import React from "react";
-import logo from "../img/mr.svg";
 import { Spotify } from "./styled/Spotify";
 import { SoundCloud } from "./styled/SoundCloud";
 import { ApplePodcast } from "./styled/ApplePodcast";
 import { RSS } from "./styled/RSS";
-import {MR} from "./styled/MR";
+import { MR } from "./styled/MR";
 
 class Header extends React.Component {
     render() {
         return (
             <header>
-                <div className="headerContainer">
+                <div className="headerDiv">
                     <a href="/">
                         <h1>
                             <MR />
