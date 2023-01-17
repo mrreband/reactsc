0bd93225cdeafef5adba7cb04b51de84cccf8876
mrreband <mr@reband.org>
2023-01-17 09:13:38 -0600

    add dark mode stuff

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.scss";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import SoundList from "./components/SoundList";
+import DarkModeButton from "./components/styled/DarkModeButton";
 import SubscribeLink from "./components/SubscribeLink";
 
 class App extends React.Component {
@@ -14,6 +15,7 @@ class App extends React.Component {
                     <div className="PianoPodcastDiv">
                         <h2>Piano Podcast</h2>
                         <SubscribeLink />
+                        <DarkModeButton />
                     </div>
                     <SoundList />
                 </div>
---
+++ src/components/styled/DarkModeButton.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "../../themes.css";
+
+const DarkModeButton = () => {
+    const body = document.body;
+    const lightTheme = "light";
+    const darkTheme = "dark";
+    let theme;
+
+    if (localStorage) {
+        theme = localStorage.getItem("theme");
+    }
+
+    if (theme === lightTheme || theme === darkTheme) {
+        body.classList.add(theme);
+    } else {
+        body.classList.add(lightTheme);
+    }
+
+    const switchTheme = (e) => {
+        const newTheme = theme === lightTheme ? darkTheme : lightTheme;
+        body.classList.replace(theme, newTheme);
+        theme = newTheme;
+    };
+
+    return (
+        <button
+            className={theme}
+            id="darkModeButton"
+            onClick={(e) => switchTheme(e)}
+        ></button>
+    );
+};
+
+export default DarkModeButton;
